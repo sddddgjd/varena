4284ecdd51ad73cc4f934b90608e064bfe9d10dc
David Batranu <dbatranu@gmail.com>
2016-04-16 23:13:21 +0300

    [refs #2] Fixing ansible php-fpm deployment
    * correctly define php_enable_php_fpm
    * correctly define php_fpm_daemon
    * adding task to update php-fpm/pool.d/www.conf with proper permissions
      and enable error output
    * adding checks to createUser.php so it doesn't crash
      on repeated executions
    * adding checks to  grantAllPermissions.php so the database doesn't
      become polluted on subsequent runs

--- deploy/README.md
+++ deploy/README.md
@@ -2,6 +2,9 @@
 - install [Vagrant](https://www.vagrantup.com/downloads.html)
 - install [Ansible](https://docs.ansible.com/ansible/intro_installation.html)
 - run ./setup.sh
+- wait a few minutes for setup to complete
+- if all goes well http://localhost:1080 should point to a working deployment.
+- login with username: *admin@localhost* and password *admin*
 - `vagrant provision` to re-run the ansible setup.
 
 If you screw something up just `vagrant destroy jessie` and `vagrant up`.
--- scripts/createUser.php
+++ scripts/createUser.php
@@ -6,11 +6,19 @@ if ($argc != 4) {
   die("Usage: {$argv[0]} <username> <email> <password>\n");
 }
 
-$u = Model::factory('User')->create();
-$u->username = $argv[1];
-$u->email = $argv[2];
-$u->password = password_hash($argv[3], PASSWORD_DEFAULT);
-$u->save();
+try {
+  $u = Model::factory('User')->create();
 
-print "User {$u->username} (ID = {$u->id}) created!.\n";
+  $u->username = $argv[1];
+  $u->email = $argv[2];
+  $u->password = password_hash($argv[3], PASSWORD_DEFAULT);
+
+  $u->save();
+
+  print "User {$u->username} (ID = {$u->id}) created!.\n";
+
+} catch (Exception $e) {
+  print "ERROR: {$e->getMessage()}\n";
+  print "User not created due to error!\n";
+}
 
