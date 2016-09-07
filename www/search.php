3319a23740f84165f62b2af51fe642b852e2ff39
Zloteanu Anastasia <zlot.anastasia@gmail.com>
2016-09-07 13:43:48 +0300

    Minor changes on search

--- www/search.php
+++ www/search.php
@@ -1,90 +1,85 @@
 <?php
 
-class Search
-{
-  
-  public static $searchMinLength = 4;
-  public static $searchMaxLength = 30;
-  
-  public static $nameScore = 30;
-  public static $authorScore = 20;
-  public static $tagScore = 10;
-  public static $statementScore = 1;
-  
-  private static $searchQueries;
+$searchMinLength = 4;
+$searchMaxLength = 30;
+$nameScore = 30;
+$authorScore = 20;
+$tagScore = 10;
+$statementScore = 1;
+$searchQueries;
 
-  static function validateSearch($search){
-
-    if (strlen($search) > self::$searchMaxLength) {
-      FlashMessage::add(_('Search term too long'));
-      Http::redirect('index.php');
-    }
-    
-    foreach (self::$searchQueries as $key=>$query){
-      if (strlen($query) < self::$searchMinLength) {
-        unset(self::$searchQueries[$key]);
-      }
-    }
-
-    if (!count(self::$searchQueries)) {
-      FlashMessage::add(_('Search words too short'));
-      Http::redirect('index.php');
-    }
-  }
-  
-  static function printResults($scores){
-    arsort($scores);
-    $results = [];
-
-    foreach ($scores as $key=>$score)
-      array_push($results,Problem::get_by_id($key));
-
-    SmartyWrap::assign('results',$results);
-    SmartyWrap::display('search.tpl');
+function validateSearch($search){
+  if (strlen($search) > $GLOBALS['searchMaxLength']) {
+    FlashMessage::add(_('Search term too long'));
+    Http::redirect('index.php');
   }
 
-  static function searchQuery($pdo) {
-    $scores = array();
-    $searchFields = ['name','author','statement'];
-    $fieldScore = array_combine($searchFields, array(self::$nameScore, self::$authorScore, self::$statementScore));
-
-    foreach (self::$searchQueries as $eachQuery) {
-      foreach ($searchFields as $field) {
-        $problems = $pdo->query("SELECT * FROM problem WHERE $field LIKE '%$eachQuery%'");
-
-        foreach ($problems as $problem)
-          @$scores[$problem['id']] += $fieldScore[$field];
-      }
-      
-      $problems = $pdo->query("SELECT * FROM tag WHERE value LIKE '%$eachQuery%'");
-
-      foreach ($problems as $problem) {
-        $id = $problem['id'];
-        $newQuery = "SELECT * FROM problem_tag WHERE tagId = $id";
-        $problemTags = $pdo->query($newQuery);
-
-        foreach ($problemTags as $problemTag)
-          @$scores[$problemTag['problemId']] += self::$tagScore;
-      }
-    }
-
-    self::printResults($scores);
+  if(strlen($search) < $GLOBALS['searchMinLength']) {
+   FlashMessage::add(_('Search term too short'));
+   Http::redirect('index.php');
   }
-  
-  static function init() {
-    require_once '../lib/Util.php';
 
-    $search = Request::get('search');
-    self::$searchQueries = explode(" ", $search);
-    self::validateSearch($search);
+  foreach($GLOBALS['searchQueries'] as $key => $query) {
+    if (strlen($query) < $GLOBALS['searchMinLength']) {
+      unset($GLOALS['searchQueries[$key]']);
+    }
+  }
 
-    $dsn = Db::parseDsn(Config::get('general.database'));
-    $pdo = new PDO('mysql:host=' . $dsn['host'] . ';dbname=varena', $dsn['user'], $dsn['password']);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    self::searchQuery($pdo);
+  if (!count($GLOBALS['searchQueries'])) {
+    FlashMessage::add(_('Search words too short'));
+    Http::redirect('index.php');
   }
 }
 
-Search::init();
+function printResults($scores){
+  arsort($scores);
+  $results = [];
+  foreach($scores as $key => $score) array_push($results, Problem::get_by_id($key));
+  SmartyWrap::assign('results', $results);
+  SmartyWrap::display('search.tpl');
+}
+
+function searchQuery($pdo){
+  $scores = array();
+  $searchFields = ['name', 'author', 'statement'];
+  $fieldScore = array_combine($searchFields, array(
+    $GLOBALS['nameScore'],
+    $GLOBALS['authorScore'],
+    $GLOBALS['statementScore']
+  ));
+  foreach($GLOBALS['searchQueries'] as $eachQuery) {
+    foreach($searchFields as $field) {
+      $problems = $pdo->query("SELECT * FROM problem WHERE $field LIKE '%$eachQuery%'");
+      foreach($problems as $problem) @$scores[$problem['id']]+= $fieldScore[$field];
+    }
+
+    $problems = $pdo->query("SELECT * FROM tag WHERE value LIKE '%$eachQuery%'");
+    foreach($problems as $problem) {
+      $id = $problem['id'];
+      $newQuery = "SELECT * FROM problem_tag WHERE tagId = $id";
+      $problemTags = $pdo->query($newQuery);
+      foreach($problemTags as $problemTag) @$scores[$problemTag['problemId']]+= $GLOBALS['tagScore'];
+    }
+  }
+
+  printResults($scores);
+}
+
+require_once '../lib/Util.php';
+
+$searchMinLength = Config::get('search.searchMinLength');
+$searchMaxLength = Config::get('search.searchMaxLength');
+$nameScore = Config::get('search.nameScore');
+$authorScore = Config::Get('search.authorScore');
+$tagScore = Config::get('search.tagScore');
+$statementScore = Config::get('search.statementScore');
+
+$search = Request::get('search');
+$searchQueries = explode(" ", $search);
+validateSearch($search);
+$dsn = Db::parseDsn(Config::get('general.database'));
+$pdo = new PDO('mysql:host=' . $dsn['host'] . ';dbname=varena', $dsn['user'], $dsn['password']);
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+searchQuery($pdo);
+
 ?>
